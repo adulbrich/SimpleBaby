ca04671bd8a96aa4f1c834b0ba194ac283eea3f4
Mikenunz1 <mikenunzioo@gmail.com>
2025-11-21 16:10:15 -0800

    Sounds for Tracker Buttons
    Added sound effects for the tracker buttons. I think this will be a good place to pause on adding sound for this branch so I have time to find larger variety of sound effects and so I can implment sound settings.

--- components/tracker-button.tsx
+++ components/tracker-button.tsx
@@ -1,5 +1,6 @@
 import { ExternalPathString, router } from 'expo-router';
 import { TouchableOpacity, Text } from 'react-native';
+import { useAudioPlayer } from 'expo-audio';
 
 /**
  * TrackerButton component renders a stylized button with an icon and label.
@@ -13,11 +14,21 @@ type Button = {
     link: ExternalPathString
 }
 
+const buttonSound = require('../assets/sounds/ui-pop.mp3')
+const player = useAudioPlayer(buttonSound);
+
 export default function TrackerButton({ button, testID }: { button: Button, testID?: string }) {
     return (
+
+
+        
         <TouchableOpacity
             className='tracker-button'
-            onPress={() => router.push(button.link)}
+            onPress={() => {
+                router.push(button.link);
+                player.seekTo(0);
+                player.play();
+            }}
             testID={testID}
         >
             <Text className='tracker-icon'>{button.icon}</Text>
