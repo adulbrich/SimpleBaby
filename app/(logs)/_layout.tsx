21dc36027e500725cece86d3bdb89fc49b63a3da
Mikenunz1 <mikenunzioo@gmail.com>
2025-12-03 12:06:18 -0800

    Adds sound to the log page's back button

--- app/(logs)/_layout.tsx
+++ app/(logs)/_layout.tsx
@@ -1,16 +1,23 @@
 import { router, Stack } from 'expo-router';
-import {
-    Text,
-    TouchableOpacity,
-} from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
+import { useAudioPlayer } from 'expo-audio';
 
 export default function LogsLayout() {
+
+    //These const variables get the sound file and an AudioPlayer isntance to play it
+    const buttonSound = require('../../assets/sounds/ui-back.mp3');
+    const player = useAudioPlayer(buttonSound);
+
     return (
         <Stack
             screenOptions={{
                 headerLeft: () => (
                     <TouchableOpacity
                         onPress={() => {
+
+                            //Resets the position of the sound effect to the beginning of the audio file and plays
+                            player.seekTo(0);
+                            player.play();
                             router.dismissTo('/(tabs)/trends');
                         }}
                         className='dark:bg-slate-700 bg-blue-200 p-2 rounded-xl border-[1px] border-blue-300 dark:border-slate-600 android:mr-4'
