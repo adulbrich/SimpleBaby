6ff254183fabd026c1db55cb61b980e65698c5d5
granjame <jlg20045@gmail.com>
2025-12-30 16:52:53 -0800

    Added tests for app/(trackers)/health.tsx

--- test/app/(trackers)/health.test.tsx
+++ test/app/(trackers)/health.test.tsx
@@ -14,7 +14,7 @@ jest.mock("expo-router", () => ({
 }));
 
 jest.mock("@/library/supabase-client", () => {
-    const insert = jest.fn();
+    const insert = jest.fn(async () => ({ error: false }));
     return ({
         from: () => ({
             insert: insert,
@@ -23,7 +23,7 @@ jest.mock("@/library/supabase-client", () => {
 });
 
 jest.mock("@/library/crypto", () => ({
-    encryptData: jest.fn(async () => "")
+    encryptData: jest.fn(async (string) => `Encrypted: ${string}`)
 }));
 
 jest.mock("react-native", () => {
@@ -46,26 +46,43 @@ jest.mock("@/components/health-module.tsx", () => {
  *  setHealthInputs:
  *      Reads update handlers from first call to HealthModule mock
  *      Calls update handlers with provided inputs
- *      Cuurently only supports category and growth updates
 */
-function setHealthInputs({
+async function setHealthInputs({
     category,
     growth,
+    activity,
+    meds,
+    date,
 } : {
-    category?: HealthCategory,
-    growth?: { length?: string; weight?: string; head?: string; },
+    category?: HealthCategory;
+    growth?: { length?: string; weight?: string; head?: string; };
+    activity?: { type: string; duration: string; };
+    meds?: { name?: string; amount?: string; timeTaken?: Date; };
+    date?: Date,
 }) {
     // read parameters to first call of HealthModule
     const {
         onCategoryUpdate,
         onGrowthUpdate,
+        onActivityUpdate,
+        onMedsUpdate,
+        onDateUpdate,
     } = (HealthModule as jest.Mock).mock.calls[0][0];
 
     if (category) {
-        act(() => onCategoryUpdate?.(category));
+        await act(() => onCategoryUpdate?.(category));
     }
     if (growth) {
-        act(() => onGrowthUpdate?.(growth));
+        await act(() => onGrowthUpdate?.(growth));
+    }
+    if (activity) {
+        await act(() => onActivityUpdate?.(activity));
+    }
+    if (meds) {
+        await act(() => onMedsUpdate?.(meds));
+    }
+    if (date) {
+        await act(() => onDateUpdate?.(date));
     }
 }
 
@@ -75,6 +92,7 @@ describe("Track health screen", () => {
         // to clear the .mock.calls arrays
         (HealthModule as jest.Mock).mockClear();
         (Alert.alert as jest.Mock).mockClear();
+        (supabase.from("").insert as jest.Mock).mockClear();
         jest.spyOn(console, "error").mockClear();
     });
 
@@ -90,6 +108,29 @@ describe("Track health screen", () => {
 
         expect(screen.getByTestId("health-save-log-button")).toBeTruthy();
         expect(screen.getByTestId("health-reset-form-button")).toBeTruthy();
+    });
+
+    test("Refreshes on reset", async () => {
+        const testNote = "test note";
+        render(<Health/>);
+
+        // write something in the note entry...
+        await userEvent.type(
+            screen.getByTestId("health-note-entry"),
+            testNote
+        );
+        expect(screen.getByDisplayValue(testNote)).toBeTruthy();  // ensure the typed note can be found
+
+        const mainInputs = screen.getByTestId("health-main-inputs");  // get the displayed <HealthModule/>
+
+        await userEvent.press(
+            screen.getByTestId("health-reset-form-button")
+        );
+
+        // ensure note is no longer present
+        expect(() => screen.getByText(testNote)).toThrow("Unable to find an element with text: test note");
+        // ensure new instance of <HealthModule/> is being used
+        expect(screen.getByTestId("health-main-inputs") === mainInputs).toBeFalsy();
     });
         
     test("Catch unfilled growth inputs", async () => {
@@ -107,7 +148,7 @@ describe("Track health screen", () => {
     test("Catch unfilled activity inputs", async () => {
         render(<Health/>);
 
-        setHealthInputs({category: "Activity"});  // switch to activity category
+        await setHealthInputs({category: "Activity"});  // switch to activity category
 
         await userEvent.press(
             screen.getByTestId("health-save-log-button")
@@ -121,7 +162,7 @@ describe("Track health screen", () => {
     test("Catch unfilled meds inputs", async () => {
         render(<Health/>);
 
-        setHealthInputs({category: "Meds"});  // switch to meds category
+        await setHealthInputs({category: "Meds"});  // switch to meds category
 
         await userEvent.press(
             screen.getByTestId("health-save-log-button")
@@ -144,7 +185,7 @@ describe("Track health screen", () => {
     
         render(<Health/>);
 
-        setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
+        await setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
         await userEvent.press(
             screen.getByTestId("health-save-log-button")
         );
@@ -168,7 +209,7 @@ describe("Track health screen", () => {
     
         render(<Health/>);
 
-        setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
+        await setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
         await userEvent.press(
             screen.getByTestId("health-save-log-button")
         );
@@ -198,7 +239,7 @@ describe("Track health screen", () => {
     
         render(<Health/>);
 
-        setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
+        await setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
         await userEvent.press(
             screen.getByTestId("health-save-log-button")
         );
@@ -214,17 +255,10 @@ describe("Track health screen", () => {
         expect((Alert.alert as jest.Mock).mock.calls[0][1]).toBe(`Failed to save health log: ${testErrorMessage}`);
     });
         
-    test("Successfully submit health log", async () => {
-        // supabase.from().insert() should be mocked to return:
-        // { error: /* falsy value */ }
-        // This should not cause any errors
-        (supabase.from("").insert as jest.Mock).mockReturnValueOnce(
-            Promise.resolve({ error: false })
-        );
-    
+    test("Redirects user on successful submit", async () => {
         render(<Health/>);
 
-        setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
+        await setHealthInputs({growth: {length: "x", weight: "", head: ""}});  // fill in any input
         await userEvent.press(
             screen.getByTestId("health-save-log-button")
         );
@@ -236,5 +270,124 @@ describe("Track health screen", () => {
         // Alert.alert() called by app/(trackers)/health.tsx -> handleSaveHealthLog()
         expect((Alert.alert as jest.Mock).mock.calls[0][0]).toBe(`Success`);
         expect((Alert.alert as jest.Mock).mock.calls[0][1]).toBe(`Health log saved successfully!`);
+    });
+        
+    test("Saves correct values (growth)", async () => {
+        const testLength = "test length";
+        const testWeight = "test weight";
+        const testHead = "test head";
+        render(<Health/>);
+
+        // fill inputs
+        await setHealthInputs({growth: {length: testLength, weight: testWeight, head: testHead}});
+
+        // submit log
+        await userEvent.press(
+            screen.getByTestId("health-save-log-button")
+        );
+
+        const insertedObject = (supabase.from("").insert as jest.Mock).mock.calls[0][0][0];
+
+        // Ensure Supabase.insert was called with the correct values, which should now be encrypted
+        expect(insertedObject.category).toBe("Growth");
+        expect(insertedObject.growth_length).toBe(await encryptData(testLength));
+        expect(insertedObject.growth_weight).toBe(await encryptData(testWeight));
+        expect(insertedObject.growth_head).toBe(await encryptData(testHead));
+
+        // Ensure that log was saved successfully
+        // Alert.alert() called by app/(trackers)/health.tsx -> handleSaveHealthLog()
+        expect((Alert.alert as jest.Mock).mock.calls[0][0]).toBe(`Success`);
+    });
+        
+    test("Saves correct values (activity)", async () => {
+        const testType = "test activity";
+        const testDuration = "test duration";
+        render(<Health/>);
+
+        // fill inputs
+        await setHealthInputs({category: "Activity", activity: {type: testType, duration: testDuration}});
+
+        // submit log
+        await userEvent.press(
+            screen.getByTestId("health-save-log-button")
+        );
+
+        const insertedObject = (supabase.from("").insert as jest.Mock).mock.calls[0][0][0];
+
+        // Ensure Supabase.insert was called with the correct values, which should now be encrypted
+        expect(insertedObject.category).toBe("Activity");
+        expect(insertedObject.activity_type).toBe(await encryptData(testType));
+        expect(insertedObject.activity_duration).toBe(await encryptData(testDuration));
+
+        // Ensure that log was saved successfully
+        // Alert.alert() called by app/(trackers)/health.tsx -> handleSaveHealthLog()
+        expect((Alert.alert as jest.Mock).mock.calls[0][0]).toBe(`Success`);
+    });
+        
+    test("Saves correct values (meds)", async () => {
+        const testName = "test name";
+        const testAmount = "test amount";
+        const testTime = new Date;
+        render(<Health/>);
+
+        // fill inputs
+        await setHealthInputs({category: "Meds", meds: {name: testName, amount: testAmount, timeTaken: testTime}});
+
+        // submit log
+        await userEvent.press(
+            screen.getByTestId("health-save-log-button")
+        );
+
+        const insertedObject = (supabase.from("").insert as jest.Mock).mock.calls[0][0][0];
+
+        // Ensure Supabase.insert was called with the correct values, which should now be encrypted
+        expect(insertedObject.category).toBe("Meds");
+        expect(insertedObject.meds_name).toBe(await encryptData(testName));
+        expect(insertedObject.meds_amount).toBe(await encryptData(testAmount));
+        expect(insertedObject.meds_time_taken).toBe(testTime);
+
+        // Ensure that log was saved successfully
+        // Alert.alert() called by app/(trackers)/health.tsx -> handleSaveHealthLog()
+        expect((Alert.alert as jest.Mock).mock.calls[0][0]).toBe(`Success`);
+    });
+        
+    test("Saves correct values (id/date/note)", async () => {
+        const testNote = "test note";
+        const testDate = new Date;
+        const testID = "test ID";
+
+        // mock library/utils.ts -> getActiveChildId() to return the test child ID
+        (getActiveChildId as jest.Mock).mockImplementationOnce(
+            async () => ({ success: true, childId: testID })
+        );
+
+        render(<Health/>);
+
+        // write a note
+        await userEvent.type(
+            screen.getByTestId("health-note-entry"),
+            testNote
+        );
+        // fill date
+        await setHealthInputs({
+            date: testDate,
+            growth: {length: "x", weight: "", head: ""}  // fill in minum required inputs to avoid errors
+        });
+
+        // submit log
+        await userEvent.press(
+            screen.getByTestId("health-save-log-button")
+        );
+
+        const insertedObject = (supabase.from("").insert as jest.Mock).mock.calls[0][0][0];
+
+        // Ensure Supabase.insert was called with the correct values; the note should now be encrypted
+        expect(insertedObject.child_id).toBe(testID);
+        expect(insertedObject.date).toBe(testDate);
+        expect(insertedObject.note).toBe(await encryptData(testNote));
+
+        // Ensure that log was saved successfully
+        // Alert.alert() called by app/(trackers)/health.tsx -> handleSaveHealthLog()
+        expect((Alert.alert as jest.Mock).mock.calls[0][0]).toBe(`Success`);
     });
 });
