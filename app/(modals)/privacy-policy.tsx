d91bfd86677015e0a6c4a8c884bbda40eeae590c
Mikenunz1 <mikenunzioo@gmail.com>
2026-05-09 14:42:29 -0700

    Privacy Policy First Draft
    Uploaded the first draft of the privacy policy into the privacy policy section

--- app/(modals)/privacy-policy.tsx
+++ app/(modals)/privacy-policy.tsx
@@ -14,17 +14,18 @@ export default function PrivacyPolicy() {
         <ScrollView>
             <View className='modal-container flex-col justify-between flex-grow'>
                 <Text className="tos-heading">{stringLib.privacyPolicy.setionHeader.about}</Text>
-                <Text className="tos-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis magna vitae nisi ullamcorper, sit amet dignissim felis fermentum. Mauris sed pharetra nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a cursus eros. Ut ut justo vestibulum eros dictum blandit eu non diam. Nam sem dolor, aliquam nec mattis vel, tincidunt ultricies eros. Morbi fermentum odio at tortor tempus, a aliquet dui placerat. Vivamus eu vehicula magna. </Text>
-                <Text className='tos-text'>orem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis magna vitae nisi ullamcorper, sit amet dignissim felis fermentum. Mauris sed pharetra nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a cursus eros. Ut ut justo vestibulum</Text>
+                <Text className="tos-text">{stringLib.privacyPolicy.content.about}</Text>
                 <Text className="tos-heading">{stringLib.privacyPolicy.setionHeader.dataUse}</Text>
-                <Text className='tos-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis magna vitae nisi ullamcorper, sit amet dignissim felis fermentum. Mauris sed pharetra nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a cursus eros. Ut ut justo vestibulum eros dictum blandit eu non diam. Nam sem dolor, aliquam nec mattis vel, tincidunt ultricies eros. Morbi fermentum odio at tortor tempus, a aliquet dui placerat. Vivamus eu vehicula magna. </Text>
+                <Text className="tos-text">{stringLib.privacyPolicy.content.dataUse1}</Text>
+                <Text className="tos-text">{stringLib.privacyPolicy.content.dataUse2}</Text>
                 <Text className="tos-heading">{stringLib.privacyPolicy.setionHeader.permissions}</Text>
-                <Text className='tos-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis magna vitae nisi ullamcorper, sit amet dignissim felis fermentum. Mauris sed pharetra nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a cursus eros. Ut ut justo vestibulum eros dictum blandit eu non diam. Nam sem dolor, aliquam nec mattis vel, tincidunt ultricies eros. Morbi fermentum odio at tortor tempus, a aliquet dui placerat. Vivamus eu vehicula magna. </Text>
+                <Text className="tos-text">{stringLib.privacyPolicy.content.permissions}</Text>
                 <Text className="tos-heading">{stringLib.privacyPolicy.setionHeader.subaBase}</Text>
-                <Text className='tos-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis magna vitae nisi ullamcorper, sit amet dignissim felis fermentum. Mauris sed pharetra nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a cursus eros. Ut ut justo vestibulum eros dictum blandit eu non diam. Nam sem dolor, aliquam nec mattis vel, tincidunt ultricies eros. Morbi fermentum odio at tortor tempus, a aliquet dui placerat. Vivamus eu vehicula magna. </Text>
-                <Text className='tos-text'>orem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis magna vitae nisi ullamcorper, sit amet dignissim felis fermentum. Mauris sed pharetra nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a cursus eros. Ut ut justo vestibulum</Text>
+                <Text className="tos-text">{stringLib.privacyPolicy.content.subaBase}</Text>
                 <Text className="tos-heading">{stringLib.privacyPolicy.setionHeader.changes}</Text>
+                <Text className="tos-text">{stringLib.privacyPolicy.content.changes}</Text>
                 <Text className="tos-heading">{stringLib.privacyPolicy.setionHeader.contact}</Text>
+                <Text className="tos-text">{stringLib.privacyPolicy.content.contant}</Text>
             </View>
         </ScrollView>
     );
