5a7946943b18b480eb99687b6b2892bb29093415
NathanJohnstonn <132169663+NathanJohnstonn@users.noreply.github.com>
2026-05-11 15:33:03 -0700

    fix test warnings

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,18 @@
 import '@testing-library/jest-native/extend-expect';
 import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 
+jest.mock('@expo/vector-icons', () => ({
+  Ionicons: 'Ionicons',
+  MaterialIcons: 'MaterialIcons',
+  MaterialCommunityIcons: 'MaterialCommunityIcons',
+  Entypo: 'Entypo',
+  FontAwesome: 'FontAwesome',
+  FontAwesome5: 'FontAwesome5',
+  FontAwesome6: 'FontAwesome6',
+  AntDesign: 'AntDesign',
+  SimpleLineIcons: 'SimpleLineIcons',
+}));
+
 jest.mock('react-native-reanimated', () =>
     require('react-native-reanimated/mock')
 );
